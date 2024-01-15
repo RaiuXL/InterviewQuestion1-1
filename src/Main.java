226161fdc05e0572b0c7ce3b30215459d44cce68
Nathan Tapia <RaiuXL@noreply.github.com>
2024-01-15 10:17:15 -0800

    Finished isWordUnique

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.util.HashMap;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
@@ -12,10 +14,20 @@ public class Main {
             System.out.println("i = " + i);
         }
 
-        String word = "Katsu";
+        String word = "Hey";
+        System.out.println(isWordUnique(word));
     }
-    public boolean isWordUnique(String word){
+    public static boolean isWordUnique(String word){
+        HashMap<Character, Integer> wordMap = new HashMap<>();
 
-        return false;
+        for(char c: word.toCharArray()){
+            if(wordMap.containsKey(c)){
+                return false;
+            } else{
+                wordMap.put(c,1);
+            }
+        }
+
+        return true;
     }
 }
